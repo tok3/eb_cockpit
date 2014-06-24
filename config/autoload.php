55aea5926de3ed8310bfaee984ea5797235cf95f
Tobias C. Koch <Tobias@eq3w.de>
2014-06-24 16:44:03 +0200

    form lib aus autoload

--- config/autoload.php
+++ config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('format','session','table','form');
+$autoload['libraries'] = array('format','session','table');
 
 
 /*
