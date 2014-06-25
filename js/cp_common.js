a4759f1f7f6f850a263a9415a72ef7886c43dcdf
Tobias C. Koch <Tobias@eq3w.de>
2014-06-25 17:36:15 +0200

    app.js to cp_common.js

---
+++ js/cp_common.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	var contact_id = ci_session.getSessionData('contact_id');
+
+	/**
+	 * nach dem ersten anmelden 
+	 * 
+	 */
+	sideNavState('.noauth','disabled');
+	if(contact_id > 0) // kontaktdaten wurden ausgefüllt
+	{
+		$('.notifications-menu').removeClass('hide');
+	}
+	else
+	{
+		sideNavState('.actLevel2','disabled');
+			$('.small-box-footer').removeAttr("href");
+	}
+
+
+
+
+
+
+/**
+* item in sidenav deaktivieren oder entfernen
+* 
+*/
+	function sideNavState(sel,state)
+	{
+
+		if(state === 'disabled')
+		{
+			$(sel+' a').removeAttr("href")
+				.css("text-align", "left")
+				.addClass('btn  disabledbtn  disabled');
+		}
+		if(state === 'remove')
+		{
+			$(sel).remove();
+
+		}
+	}
+
+	// --------------------------------------------------------------------
+});
