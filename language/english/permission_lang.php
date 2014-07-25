1a25ff52af5ddad7d2445f2d74810a8c29d4bbc6
Tobias C. Koch <Tobias@eq3w.de>
2014-07-25 12:52:35 +0200

    permission_lang added

---
+++ language/english/permission_lang.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Journal Permissions
+$lang['cockpit:role_customer']		= 'Kunde';
+$lang['cockpit:role_supervisor']		= 'Supervisor';
+$lang['cockpit:role_affiliate']	= 'Affilliate (test)';
+$lang['cockpit:role_handelsvertreter']	= 'Handelsvertreter (test)';
+
+/* End of file permission_lang.php */
